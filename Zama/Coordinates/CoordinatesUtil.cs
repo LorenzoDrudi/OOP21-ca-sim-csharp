c3ddeda978fc8e81591749e07f96ccd73908a64b
Simone Zama <simone.zama@studio.unibo.it>
2022-04-29 12:44:30 +0200

    Implement `Coordinates3D` utility methods

--- Zama/Coordinates/CoordinatesUtil.cs
+++ Zama/Coordinates/CoordinatesUtil.cs
@@ -26,9 +26,10 @@ namespace Zama.Coordinates
         /// <param name="bottomRight"></param> the bottom right angle.
         /// <returns></returns> true if coord is inside the given limits, false otherwise.
         public static bool IsValid(Coordinates2D coord, Coordinates2D topLeft, Coordinates2D bottomRight) =>
-            coord.X.CompareTo(topLeft.X) >= 0 && coord.Y.CompareTo(topLeft.Y) >= 0
-                                              && coord.X.CompareTo(bottomRight.X) < 0
-                                              && coord.Y.CompareTo(bottomRight.Y) < 0;
+            coord.X.CompareTo(topLeft.X) >= 0 && 
+            coord.Y.CompareTo(topLeft.Y) >= 0 && 
+            coord.X.CompareTo(bottomRight.X) < 0 && 
+            coord.Y.CompareTo(bottomRight.Y) < 0;
 
         /// <summary>
         /// Checks if the given <see cref="Coordinates2D"/> is inside the rectangle
@@ -53,7 +54,7 @@ namespace Zama.Coordinates
             IsValid(coord, Of(maxX, maxY));
 
         /// <summary>
-        /// Returns the Von Neuman's neighbors of the <see cref="Coordinates2D"/>
+        /// Returns the Von Neumann's neighbors of the <see cref="Coordinates2D"/>
         /// given as argument.
         /// </summary>
         /// <param name="coord"></param> the <see cref="Coordinates2D"/> of which to calculate
@@ -71,9 +72,10 @@ namespace Zama.Coordinates
         /// </summary>
         /// <param name="maxX"></param> the upper limit of the X coordinate must be >= 0.
         /// <param name="maxY"></param> the upper limit of the Y coordinate must be >= 0.
-        /// <returns></returns>
-        public static Coordinates2D random(int maxX, int maxY) =>
-            Of(new Random().Next(0, maxX), new Random().Next(0, maxY));
+        /// <returns></returns> a new <see cref="Coordinates2D"/> with random values within
+        ///         given limits,
+        public static Coordinates2D Random(int maxX, int maxY) =>
+            Of(new Random().Next(maxX), new Random().Next(maxY));
 
         /// <summary>
         /// Creates a <see cref="Coordinates3D"/>.
@@ -83,5 +85,65 @@ namespace Zama.Coordinates
         /// <param name="z"></param> the value to be used as the z coordinate.
         /// <returns></returns> a <see cref="Coordinates3D"/> with argument values.
         public static Coordinates3D Of(int x, int y, int z) => new Coordinates3D(x, y, z);
+
+        /// <summary>
+        /// Checks if <see cref="coord"/> is inside the rectangle formed by
+        /// <see cref="topLeft"/> and <see cref="bottomRight"/>.
+        /// </summary>
+        /// <param name="coord"></param> the <see cref="Coordinates3D"/> to test.
+        /// <param name="topLeft"></param> the top left angle of the rectangle.
+        /// <param name="bottomRight"></param> the bottom right angle of the rectangle.
+        /// <returns></returns> true if <see cref="coord"/> is inside the limits, false otherwise.
+        public static bool IsValid(Coordinates3D coord, Coordinates3D topLeft, Coordinates3D bottomRight) =>
+            coord.X.CompareTo(topLeft.X) >= 0 && coord.Y.CompareTo(topLeft.Y) >= 0 &&
+            coord.Z.CompareTo(topLeft.Z) >= 0 && coord.X.CompareTo(bottomRight.X) < 0 &&
+            coord.Y.CompareTo(bottomRight.Y) < 0 && coord.Z.CompareTo(bottomRight.Z) < 0;
+
+        /// <summary>
+        /// Checks if <see cref="coord"/> is inside the rectangle formed by
+        /// <see cref="bottomRight"/> and (0, 0, 0) as the top left angle.
+        /// </summary>
+        /// <param name="coord"></param> the <see cref="Coordinates3D"/> to test.
+        /// <param name="bottomRight"></param> the bottom right angle of the rectangle.
+        /// <returns></returns> true if <see cref="coord"/> is inside the limits, false otherwise.
+        public static bool IsValid(Coordinates3D coord, Coordinates3D bottomRight) =>
+            IsValid(coord, Of(0, 0, 0), bottomRight);
+
+        /// <summary>
+        /// Checks the values of <see cref="coord"/> are less than <see cref="maxX"/>,
+        /// <see cref="maxY"/> and <see cref="maxZ"/> respectively.
+        /// </summary>
+        /// <param name="coord"></param> the <see cref="Coordinates3D"/> to test.
+        /// <param name="maxX"></param> the upper limit of the X coordinate.
+        /// <param name="maxY"></param> the upper limit of the Y coordinate.
+        /// <param name="maxZ"></param> the upper limit of the Z coordinate.
+        /// <returns></returns> true if all the limits are respected, false otherwise.
+        public static bool IsValid(Coordinates3D coord, int maxX, int maxY, int maxZ) =>
+            IsValid(coord, Of(maxX, maxY, maxZ));
+
+        /// <summary>
+        /// Returns the Von Neumann's neighbors of <see cref="coord"/>.
+        /// </summary>
+        /// <param name="coord"></param> the <see cref="Coordinates2D"/> of which
+        ///         to calculate the neighbors.
+        /// <returns></returns> a <see cref="List{Coordinates3D}"/> containing the neighbors
+        ///         of <see cref="coord"/>.
+        public static List<Coordinates3D> Get3DNeighbors(Coordinates3D coord) =>
+            new List<Coordinates3D>
+                    {Of(1, 0, 0), Of(0, 1, 0), Of(0, 0, 1), 
+                        Of(-1, 0, 0), Of(0, -1, 0), Of(0, 0, -1)}
+                .Select(n => n + coord)
+                .ToList();
+
+        /// <summary>
+        /// Returns a <see cref="Coordinates3D"/> between 0 (inclusive) and
+        /// the limits given as arguments.
+        /// </summary>
+        /// <param name="maxX"></param> the upper limit of the X coordinate, must be >= 0.
+        /// <param name="maxY"></param> the upper limit of the Y coordinate, must be >= 0.
+        /// <param name="maxZ"></param> the upper limit of the Z coordinate, must be >= 0.
+        /// <returns></returns> a new <see cref="Coordinates3D"/> with values within given limits.
+        public static Coordinates3D Random(int maxX, int maxY, int maxZ) => 
+            Of(new Random().Next(maxX), new Random().Next(maxY), new Random().Next(maxZ));
     }
 }
