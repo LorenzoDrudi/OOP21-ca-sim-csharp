9df5b8b850eafe563e6146ba8e4466f870bb76e6
Simone Zama <simone.zama@studio.unibo.it>
2022-04-29 11:54:54 +0200

    Create `Coordinates3D`

---
+++ Zama/Coordinates/Coordinates3D.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Zama.Coordinates
+{
+    public class Coordinates3D : ICoordinates<int>
+    {
+        public int X { get; }
+        public int Y { get; }
+        public int Z { get; }
+
+        public Coordinates3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        
+        public override string ToString() =>
+            $"{this.GetType().Name}(X={this.X}, Y={this.Y}, Z={this.Z})";
+
+        public override int GetHashCode() => HashCode.Combine(X, Y, Z);
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Coordinates3D) obj);
+        }
+
+        public bool Equals(Coordinates3D other) =>
+            X.Equals(other.X) && Y.Equals(other.Y) && Z.Equals(other.Z);
+
+        public static Coordinates3D operator +(Coordinates3D a, Coordinates3D b) =>
+            new Coordinates3D(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+
+        public static Coordinates3D operator -(Coordinates3D a, Coordinates3D b) =>
+            a + new Coordinates3D(-b.X, -b.Y, -b.Z);
+    }
+}
