64333652eb1a3c1b9be0da32c3c30a730dbabba7
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-04-30 16:53:43 +0200

    Add `ICell` and `AbstractCell`.

---
+++ Drudi/Cell/AbstractCell.cs
@@ -0,0 +1,16 @@
+namespace Drudi.Cell
+{
+    /// <summary>
+    /// Abstract implementation of ICell.
+    /// </summary>
+    /// <typeparam name="T">The enumeration which contains the Automaton's state.</typeparam>
+    public abstract class AbstractCell<T>: ICell<T>
+    {
+        public T State { get; protected set; }
+
+        protected AbstractCell(T state)
+        {
+            this.State = state;
+        }
+    }
+}
---
+++ Drudi/Cell/ICell.cs
@@ -0,0 +1,14 @@
+namespace Drudi.Cell
+{
+    /// <summary>
+    /// A cell of the grid.
+    /// </summary>
+    /// <typeparam name="T">The enumeration which contains the Automaton's state.</typeparam>
+    public interface ICell<out T>
+    {
+        /// <summary>
+        /// The current state of the cell.
+        /// </summary>
+        public T State { get; }
+    }
+}
