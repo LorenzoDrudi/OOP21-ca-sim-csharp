78596abd6849a3d90da61db372f0a4de27b490fb
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2022-05-02 18:57:40 +0200

    Create 'IGrid2D' interface

---
+++ Chiasserini/Grid/IGrid2D.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Zama.Coordinates;
+
+namespace Chiasserini.Grid
+{
+    /// <summary>
+    /// The bidimensional grid.
+    /// </summary>
+    /// <typeparam name="T">Type of data contained in IGrid2D.</typeparam>
+    public interface IGrid2D<T> : IGrid<Coordinates2D, T>
+    {
+        /// <summary>
+        /// Return the value at given coordinates.
+        /// </summary>
+        /// <param name="row">The row of the element to get.</param>
+        /// <param name="column">The column of the element to get.</param>
+        public T Get(int row, int column);
+        
+        /// <summary>
+        /// Set an element to a given coordinates.
+        /// </summary>
+        /// <param name="row">The row of the element to set.</param>
+        /// <param name="column">The column of the element to set.</param>
+        /// <param name="value">The value to set.</param>
+        public void Set(int row, int column, T value);
+        
+        /// <summary>
+        /// Return a new IGrid2D applying a mapper function to the elements of grid.
+        /// </summary>
+        /// <param name="mapper">The map function.</param>
+        /// <typeparam name="O">The type if the element of the output grid.</typeparam>
+        /// <returns></returns>
+        public IGrid2D<O> Map<O>(Func<T, O> mapper);
+    }
+}
