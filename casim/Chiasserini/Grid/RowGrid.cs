c0d6f17a9133de3c35f19e1111e0ba3efa28a2ee
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2022-05-05 15:53:43 +0200

    Implement RowGrid

--- casim/Chiasserini/Grid/RowGrid.cs
+++ casim/Chiasserini/Grid/RowGrid.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Sanzani.RangeUtils;
 using Zama.Coordinates;
 
-namespace Chiasserini.Grid
+namespace casim.Chiasserini.Grid
 {
     public class RowGrid<T> : IGrid2D<T> where T : class
     {
@@ -27,39 +26,48 @@ namespace Chiasserini.Grid
             Ranges.Of(0, this.Width).ToList().ForEach(column => list.Add(this.Get(row, column)));
             return list;
         }
+
+        public void SetRow(int row, int column, List<T> list)
+        {
+            if (list.Count != this.Width)
+            {
+                throw new ArgumentException("Wrong list size");
+            }
+            Ranges.Of(0, list.Count).ToList().ForEach(column => this.Set(row, column, list.ElementAt(column)));
+        }
         public T Get(Coordinates2D coord)
         {
-            throw new NotImplementedException();
+            return this.Grid.Get(coord);
         }
 
         public void Set(Coordinates2D coord, T value)
         {
-            throw new NotImplementedException();
+            this.Grid.Set(coord, value);
         }
 
         public bool IsCoordValid(Coordinates2D coord)
         {
-            throw new NotImplementedException();
+            return this.Grid.IsCoordValid(coord);
         }
 
         public IQueryable<T> Stream()
         {
-            throw new NotImplementedException();
+            return this.Grid.Stream();
         }
 
         public T Get(int row, int column)
         {
-            throw new NotImplementedException();
+            return this.Grid.Get(row, column);
         }
 
         public void Set(int row, int column, T value)
         {
-            throw new NotImplementedException();
+            this.Grid.Set(row, column, value);
         }
 
         public IGrid2D<O> MapTo<O>(Func<T, O> mapper) where O : class
         {
-            throw new NotImplementedException();
+            return this.Grid.MapTo(mapper);
         }
     }
 }
