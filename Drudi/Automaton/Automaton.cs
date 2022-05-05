99dcb84a470933e35451a3a5c9bef02b0bbac0cd
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-05-05 15:29:09 +0200

    Resolve `Grid` references.

--- Drudi/Automaton/Automaton.cs
+++ Drudi/Automaton/Automaton.cs
@@ -1,22 +1,28 @@
 using System.Collections.Generic;
+using System.Linq;
+using Chiasserini.Grid;
+using Drudi.Cell;
+using Drudi.Stats;
+using Zama.Coordinates;
 
 namespace Drudi.Automaton
 {
-    public abstract class Automaton<TState, TGrid> : IAutomaton<TState, TGrid>
+    public abstract class Automaton<TCell, TState> : IAutomaton<TCell, TState> where TCell : AbstractCell<TState>
     {
         private int IterationCounter { get; set; }
-        public TGrid Grid { get; }
+        
+        public IGrid<Coordinates2D, TCell> Grid { get; }
 
-        protected Automaton(TGrid grid)
+        protected Automaton(IGrid<Coordinates2D, TCell> grid)
         {
             this.Grid = grid;
         }
 
-        //public Stats<TState> Stats => new Stats<TState>(IterationCounter, CreateStatesMap());
+        public IStats<TState> Stats => new Stats<TState>(IterationCounter, CreateStatesMap());
 
         public abstract bool HasNext();
 
-        public TGrid Next()
+        public IGrid<Coordinates2D, TCell> Next()
         {
             this.IterationCounter++;
             return DoStep();
@@ -26,11 +32,14 @@ namespace Drudi.Automaton
         /// Method used to obtain the next <see cref="Automaton{TState,TGrid}"/> step.
         /// </summary>
         /// <returns>A grid describing the next step.</returns>
-        protected abstract TGrid DoStep();
-        //TODO
-        //protected Dictionary<TState, int> CreateStatesMap()
-        //{
-        //    return Grid;
-        //}
+        protected abstract IGrid<Coordinates2D, TCell> DoStep();
+        
+        protected Dictionary<TState, int> CreateStatesMap()
+        {
+            return this.Grid.Stream()
+                .GroupBy(e => e.State)
+                .Select(e => new {Value = e.Key, Count = e.Count()})
+                .ToDictionary(e => e.Value, e => e.Count);
+        }
     }
 }
