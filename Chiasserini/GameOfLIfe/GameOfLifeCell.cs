31dc73335f25e8983014c4b783fc829ff2994e17
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2022-05-05 12:06:44 +0200

    Implement `GameOfLifeCell`

---
+++ Chiasserini/GameOfLIfe/GameOfLifeCell.cs
@@ -0,0 +1,18 @@
+﻿using Sanzani.States;
+using Zama;
+
+namespace Chiasserini.GameOfLIfe
+{
+    /// <summary>
+    /// Cell of Game Of Life.
+    /// </summary>
+    public class GameOfLifeCell : AbstractCell<GameOfLifeState>
+    {
+        /// <summary>
+        ///Package-private.
+        /// </summary>
+        /// <param name="state">the current state of the GameOfLifeCell.</param>
+        public GameOfLifeCell(GameOfLifeState state) : base(state){}
+
+    }
+}
