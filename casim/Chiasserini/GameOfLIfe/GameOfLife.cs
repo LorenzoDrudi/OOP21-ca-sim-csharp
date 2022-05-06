4832124d41115ae28d42df1a09807af9cae3f0d7
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2022-05-06 15:06:17 +0200

    Implement GameOfLife

--- casim/Chiasserini/GameOfLIfe/GameOfLife.cs
+++ casim/Chiasserini/GameOfLIfe/GameOfLife.cs
@@ -1,25 +1,51 @@
 ﻿using casim.Chiasserini.Grid;
+using casim.Drudi.AbstractionUtils;
 using casim.Drudi.Automaton;
+using casim.Zama.Coordinates;
 
 namespace casim.Chiasserini.GameOfLIfe
 {
+    /// <summary>
+    /// Class that models the Game Of Life.
+    /// </summary>
     public class GameOfLife : Automaton<GameOfLifeCell, GameOfLifeState>
     {
         private Grid2D<GameOfLifeCell> state;
-        private GameOfLifeUpdateRule updateRule = new GameOfLifeUpdateRule();
+        private GameOfLifeUpdateRule updateRule = new GameOfLifeUpdateRule(
+                    NeighborsFunctions.MooreNeighborsFunction<GameOfLifeCell, GameOfLifeState>);
         
         public GameOfLife(IGrid<Coordinates2D, GameOfLifeCell> grid) : base(grid)
         {
         }
 
+        /// <summary>
+        /// hasNext method.
+        /// </summary>
+        /// <returns>always true.</returns>
         public override bool HasNext()
         {
-            throw new System.NotImplementedException();
+            return true;
         }
 
-        protected override IGrid<Coordinates2D, TCell> DoStep()
+        /// <summary>
+        /// Function that advances of one step according to the GameOfLife's rules.
+        /// </summary>
+        /// <returns>the new state of the cell.</returns>
+        protected override IGrid<Coordinates2D, GameOfLifeCell> DoStep()
         {
-            throw new System.NotImplementedException();
+            var newState = new Grid2D<GameOfLifeCell>(this.state.Height, this.state.Width, ()=> null);
+
+            for (int x = 0; x <  this.state.Height; x++)
+            {
+                for (int y = 0; y < this.state.Width; y++)
+                {
+                    var coord = CoordinatesUtil.Of(x, y);
+                    newState.Set(coord, this.updateRule.GetNextCell(Tuple.Create(coord, this.state.Get(coord)), this.state));
+                }
+            }
+
+            this.state = newState;
+            return this.state;
         }
 
         
